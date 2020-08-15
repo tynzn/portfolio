b1a0a3cd1721134f21cd4fd007f29694261b415e
tynzn <tynznt@gmail.com>
2020-08-15 19:22:31 +0100

    getting started with my portfolio

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bakare yusuf Portfolio</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body class="">
+
+    <div class="table section">  
+        <div class="row navbar  ">
+           
+        <h2 class="col-4">Bakare yusuf</h2>
+            <a href="" class="col-2 nav-link" >About</a>
+            <a href="" class="col-2 nav-link">Projects</a>
+            <a href="" class="col-2 nav-link">Contact</a>
+        </div>
+
+        <div class="row section container">
+        
+        <div class="col-6">
+           <img class="" src="img/portfolio.jpg">
+           </div>
+           <div class="col-6 subsection">
+               <h1>About</h1>
+               <p>
+                   this is my about <section></section>
+               </p>
+           </div>
+        </div>
+        
+    </div>
+
+
+
+
+
+
+</body>
+<script src="js/bootstrap.js"></script>
+</html>
