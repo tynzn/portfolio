0c680cd6442d6cf5082ba3466e7f2f22e8872078
tynzn <tynznt@gmail.com>
2020-08-16 07:09:41 +0100

    redesigning

--- index.php
+++ index.php
@@ -5,33 +5,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bakare yusuf Portfolio</title>
 <link rel="stylesheet" href="css/bootstrap.css">
+
+<link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Lobster">
+    
+
 <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body class="">
 
     <div class="table section">  
-        <div class="row">
-           
-        <h2 class="">Bakare yusuf</h2>
+        
+       <div class="name">Bakare Yusuf</div>   
+       <br>
+<br>
+<div class="intro">
+<h5>
+        <p> 
+        am in love with writing effective code that serves your need 
+        </p>
+        <p>
+        I program Website applications and Websites using Php and Javascript  </p>
         </div>
-        <div class="row">
-            <a href="" class="col-4 nav-link" >About</a>
-            <a href="" class="col-4 nav-link">Projects</a>
-            <a href="" class="col-4 nav-link">Contact</a>
+</h5>
+        <div class="row container main-bar">
+            <a href="" class="col-4 btn link " >About</a>
+            <a href="" class="col-4 btn  link">Projects</a>
+            <a href="" class="col-4 btn link">Contact</a>
         </div>
 
-        <div class="row section container">
+        <div class="row  container">
+        <div class="col-sm-6">
+        <div class="name"> Goals</div>
+      
+           <ul>
+               <li> Developing and Delivering Services right on time in the shortest time possible</li>
+                <li>Programming and Deploying Software As A Service WebApp that ease the operation and Execution of Business related tasks</li>
+                <li> Focused on developing Marketing conversion tools and utilities </li>
+
+            </ul>
+        </div>
+        <div class="col-sm-6">
+
+        <img  src="img/portfolio.jpg" alt="">
+        </div>
+        <div class="row">
+<div class="col-sm-12 name"> Projects </div>
+
+
+        </div>
         
-        <div class="col-6">
-           <img class="" src="img/portfolio.jpg">
-           </div>
-           <div class="col-6 subsection">
-               <h1>About</h1>
-               <p>
-                   this is my about <section></section>
-               </p>
-           </div>
+
+
+           
         </div>
         
     </div>
