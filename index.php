40db6bbdcff17ee999d74ef585df24e9d4276cbc
tynzn <tynznt@gmail.com>
2020-08-17 13:42:30 +0100

    including bootstrap js

--- index.php
+++ index.php
@@ -39,15 +39,11 @@
     </div>
     -->
        <div class="name">Bakare Yusuf</div>   
-    
-<div class="intro">
+    <div class="intro">
 <h5>
-        <p> 
-        am in love with writing effective code that serves your need 
-        </p>
-        <p>
-        I program Website applications and Websites using Php and Javascript  </p>
-        </div>
+    <p> am in love with writing effective code that serves your need </p>
+     <p>I program Website applications and Websites using Php and Javascript  </p>
+</div>
 </h5>
         <div class="row container main-bar">
             <a href="" class="col-4 btn link " >About</a>
@@ -101,4 +97,7 @@
 
 </body>
 <script src="js/bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </html>
